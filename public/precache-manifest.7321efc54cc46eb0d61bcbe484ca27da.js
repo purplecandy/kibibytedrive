9dce8b9da24efd8e58c102c3fd383d591079f639
Nadeem Siddique <hellomr82k@gmail.com>
2020-12-03 10:57:31 +0530

    Added react build files

---
+++ public/precache-manifest.7321efc54cc46eb0d61bcbe484ca27da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3779327093ef7a323c34f93ab653609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7686dc6b378ca682cd5",
+    "url": "/static/css/main.9b37482e.chunk.css"
+  },
+  {
+    "revision": "51a920e2fb5e39b8274a",
+    "url": "/static/js/2.201942e3.chunk.js"
+  },
+  {
+    "revision": "541154b13c29ae6f532fc677a49b917d",
+    "url": "/static/js/2.201942e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7686dc6b378ca682cd5",
+    "url": "/static/js/main.93aec2ee.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const {Base64} = require('js-base64');
+const { Base64 } = require('js-base64');
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authentication = require("./utils.js");
 const cors = require("cors");
-
+const HOST = "https://lace-observation.glitch.me/";
 const app = express();
-app.use(cors({ origin: true,credentials: true,"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",allowedHeaders:"Cookie"}));
+app.use(cors({ origin: true, credentials: true, "methods": "GET,HEAD,PUT,PATCH,POST,DELETE", allowedHeaders: "Cookie" }));
 app.use(express.static("public"));
 app.use(
   cookieParser("w6%zk*$Y", {
@@ -14,25 +14,25 @@ app.use(
   })
 );
 
-function et(req){
-  const token =  Base64.decode(req.query.token);
-  console.log("TOKEN",token);
+function et(req) {
+  const token = Base64.decode(req.query.token);
+  console.log("TOKEN", token);
   return JSON.parse(token);
 }
-app.get("/test",(req,res)=>{
-  let jsonString = JSON.stringify({"message":Base64.encode("Hello World")});
+app.get("/test", (req, res) => {
+  let jsonString = JSON.stringify({ "message": Base64.encode("Hello World") });
   // let encoded = Base64.encode("Hello World");
   let encoded = Base64.encode(jsonString);
-  res.redirect("https://2oecc.csb.app/activate.html?code=" + encoded);
+  res.redirect(HOST + "activate.html?code=" + encoded);
   // res.json({"message":Base64.encode("Hello World")});
 });
 
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/privacy", function(request, response) {
-  response.sendFile(__dirname + "/views/privacy.html");
+app.get("/privacy", function (request, response) {
+  response.sendFile(__dirname + "/public/privacy.html");
 });
 
 app.get("/auth", (req, res) => {
@@ -43,26 +43,26 @@ app.get("/auth", (req, res) => {
   res.redirect(data.url);
 });
 
-app.get("/active", async function(req, res) {
-  try{
-  //the url has a code parameter which contains the token
-  const token = await authentication.generateToken(req.query.code, req);
-  // const token = { access_token: 'ya29.a0AfH6SMAjyfBFFImxb4kTja5huLI9cmFJuNs8aKd7Rp00dwj0z4RXTELn61efLVHd_WcPaM448GsvAHkCwTgCW3yFugPlKhkgRbPcRNy86dHAlog7SIWQCf-7NgcoiKLSiUOp2nI9QfDAJZ2xEUD_P1i3wVtE-ABeCleT4oSReKaO',
-  // scope: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.metadata',
-  // token_type: 'Bearer',
-  // expiry_date: 1606254915874 };
-  console.log(token);
-  res.cookie("dtoken", token, { secure: true });
-  const jsonString = JSON.stringify(token);
-  const encoded = Base64.encode(jsonString);
-  console.log(encoded);
-  res.redirect("https://kdrive.surge.sh/activate?code=" + encoded);    
-  }catch(e){
-    console.log("EEEEEEEEEEEEEROOR");
+app.get("/active", async function (req, res) {
+  try {
+    //the url has a code parameter which contains the token
+    const token = await authentication.generateToken(req.query.code, req);
+    // const token = { access_token: 'ya29.a0AfH6SMAjyfBFFImxb4kTja5huLI9cmFJuNs8aKd7Rp00dwj0z4RXTELn61efLVHd_WcPaM448GsvAHkCwTgCW3yFugPlKhkgRbPcRNy86dHAlog7SIWQCf-7NgcoiKLSiUOp2nI9QfDAJZ2xEUD_P1i3wVtE-ABeCleT4oSReKaO',
+    // scope: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.metadata',
+    // token_type: 'Bearer',
+    // expiry_date: 1606254915874 };
+    console.log(token);
+    res.cookie("dtoken", token, { secure: true });
+    const jsonString = JSON.stringify(token);
+    const encoded = Base64.encode(jsonString);
+    console.log(encoded);
+    res.redirect(HOST + "activate?code=" + encoded);
+  } catch (e) {
+    console.log("Error at: /active");
     console.log(e);
-    res.json({message:"Unauthenticated"});
+    res.json({ message: "Unauthenticated" });
   }
-  
+
 });
 
 app.get("/list", (req, res) => {
@@ -70,7 +70,7 @@ app.get("/list", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/file/:fileID", async (req, res)  => {
+app.get("/file/:fileID", async (req, res) => {
   await authentication.fileContent(et(req), req.params.fileID, resp => {
     res.json(resp);
   });
@@ -78,14 +78,14 @@ app.get("/file/:fileID", async (req, res)  => {
 
 //user details
 app.get("/user", async (req, res) => {
-    try {
-      let meta = await authentication.getUserMeta(et(req));
-      console.log(meta);
-      return res.status(200).json(meta);
-    } catch (e) {
-      console.log(e);
-      return res.status(400).send({ message: "Invalid Authentication" });
-    }
+  try {
+    let meta = await authentication.getUserMeta(et(req));
+    console.log(meta);
+    return res.status(200).json(meta);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ message: "Invalid Authentication" });
+  }
 });
 
 //copy file by ID
@@ -96,6 +96,6 @@ app.get("/user/file/:fileID", async (req, res) => {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
